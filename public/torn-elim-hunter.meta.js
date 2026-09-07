2c7bb0f35d82b8f7c591ba8c23d34d2019c4731d
Owen777 <hulbertowen@gmail.com>
2026-09-06 21:39:25 -0400

    feat: add userscript meta file for Tampermonkey update checks

---
+++ public/torn-elim-hunter.meta.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name         Torn Elimination Target Hunter (FF Scouter Tiers)
+// @namespace    https://spider-verse.net/
+// @version      1.1.0
+// @description  Finds beatable Elimination targets using FF Scouter Fair Fight tiers. Automatically filters out hospital, traveling, and high-FF opponents. Includes built-in test sandbox.
+// @author       Spider-Verse
+// @match        https://www.torn.com/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=torn.com
+// @grant        GM_xmlhttpRequest
+// @grant        GM_setValue
+// @grant        GM_getValue
+// @connect      ffscouter.com
+// @connect      api.torn.com
+// @run-at       document-idle
+// @updateURL    https://spider-verse.net/torn-elim-hunter.meta.js
+// @downloadURL  https://spider-verse.net/torn-elim-hunter.user.js
+// ==/UserScript==
